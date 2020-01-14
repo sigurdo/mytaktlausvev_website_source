06c7efb76207f9725d76dd0d7146a00fe326e892
Patrik Hammersborg <patrikham0@gmail.com>
2020-01-14 17:15:27 +0100

    Bedre oppsett til bruk av templating

--- site/quotes/models.py
+++ site/quotes/models.py
@@ -6,3 +6,6 @@ class Quote(models.Model):
     title = models.CharField(max_length=255)
     text = models.CharField(max_length=2000)
     timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self.text
--- site/quotes/views.py
+++ site/quotes/views.py
@@ -6,7 +6,6 @@ def all_quotes(request):
     # lager dummy-sitat til testing
     # n = Quote(title="Test", text="Test text 123", timestamp=datetime.datetime.now())
     # n.save()
-    print(Quote.objects.all())
     return render(
         request,
         "quotes/quotes.html",
