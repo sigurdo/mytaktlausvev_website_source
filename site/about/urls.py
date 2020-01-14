42a08b7a774769fc4732ef4af9fa8c71e6f0ca9e
Patrik Hammersborg <patrikham0@gmail.com>
2020-01-14 09:51:21 +0100

    Enkel templating til testprosjektet

--- site/about/urls.py
+++ site/about/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from about import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.about_us, name="about_us")
 ]
--- site/about/views.py
+++ site/about/views.py
@@ -1,7 +1,15 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("123 test test")
+# Tester bare
+def about_us(request):
+    return render(
+        request,
+        "about/about_us.html",
+        {
+            "number": random.randint(0, 10)
+        }
+    )
