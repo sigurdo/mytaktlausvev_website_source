dee1fe8ac8ed819b3fcdaf5349a4e633b9bc74d6
sigtheidiot <sigridge@stud.ntnu.no>
2020-02-19 17:46:20 +0100

    changed db from MySQL to Sqlite

--- site/web/settings.py
+++ site/web/settings.py
@@ -81,13 +81,11 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            # lag denne selv med infoen som ble brukt til å sette opp MySQL
-            'read_default_file': "web/settings.cnf"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
