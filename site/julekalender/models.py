da4cdb2aff091db2bff4ed0a4b4c786fe30052b0
Shybert <25749775+Shybert@users.noreply.github.com>
2020-02-21 08:28:20 +0100

    Set up the basics of the models required for the julekalender

--- site/julekalender/models.py
+++ site/julekalender/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Window(models.Model):
+    """Model representing a window in a julekalender"""
+
+    title = models.CharField(max_length=255)
+    post = models.TextField()
+
+
+class Julekalender(models.Model):
+    """Model representing a year's julekalender"""
+
+    year = models.IntegerField(primary_key=True)
+
+    class Meta:
+        ordering = ["-year"]
+
+
+class Windows(models.Model):
+    """Model holding the relationship between a year's julekalender and its windows"""
+
+    year = models.ForeignKey(Julekalender, on_delete=models.CASCADE)
+    window = models.ForeignKey(Window, on_delete=models.CASCADE)
+    windowNumber = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["year", "windowNumber"], name="uniqueWindow"
+            )
+        ]
