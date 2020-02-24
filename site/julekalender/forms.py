65867f8f4a11925a3c916db86b0fecebb06488e6
Shybert <25749775+Shybert@users.noreply.github.com>
2020-02-24 14:49:38 +0100

    Stopped using cripsy forms in the julekalender
    Crispy forms didn't actually add anything that Django did not

--- site/julekalender/forms.py
+++ site/julekalender/forms.py
@@ -1,17 +1,9 @@
 from django import forms
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from julekalender.models import Julekalender
 
 
 class NewJulekalenderForm(forms.ModelForm):
     """Form for creating a new julekalender"""
-
-    helper = FormHelper()
-    helper.form_id = "newJulekalender"
-    helper.form_method = "post"
-    helper.form_action = "julekalender/new"
-    helper.add_input(Submit("submit", "Ny julekalender"))
 
     class Meta:
         model = Julekalender
