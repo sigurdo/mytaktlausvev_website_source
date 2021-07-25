94f2529aa76a962b8ebccc9b11f820f14aaf3d0e
Shybert <25749775+Shybert@users.noreply.github.com>
2021-07-25 15:34:21 +0200

    Initialized Song model

--- site/songs/models.py
+++ site/songs/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    title = models.CharField("tittel", max_length=255)
+    lyrics = models.TextField("tekst")
+
+    def __str__(self):
+        return self.title
--- site/songs/tests.py
+++ site/songs/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Song
 
-# Create your tests here.
+
+class SongTestCase(TestCase):
+    def setUp(self):
+        self.song = Song.objects.create(
+            title="Song of Tests", lyrics="Test, test, test"
+        )
+
+    def test_song_to_string_equals_title(self):
+        """Song's to string method should return the song's title."""
+        self.assertEqual(str(self.song), self.song.title)
