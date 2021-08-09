88ccb1ece85f312cc62387136f4a71cebe6aa409
Shybert <25749775+Shybert@users.noreply.github.com>
2021-08-09 17:58:34 +0200

    Comments now store author and submit time

--- site/comments/models.py
+++ site/comments/models.py
@@ -1,10 +1,19 @@
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.conf import settings
 
 
 class Comment(models.Model):
-    comment = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_pk = models.CharField(max_length=64)
     content_object = GenericForeignKey("content_type", "object_pk")
+
+    comment = models.TextField("kommentar")
+    submitted = models.DateTimeField("lagt ut", auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name="laga av",
+    )
