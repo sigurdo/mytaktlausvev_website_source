297cb3f2c6d4d938000c8fc9c472e79b10c2faee
Shybert <25749775+Shybert@users.noreply.github.com>
2021-08-09 12:33:13 +0200

    Initialized Comments app and Comments model

---
+++ site/comments/models.py
@@ -0,0 +1,10 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_pk = models.CharField(max_length=64)
+    content_object = GenericForeignKey("content_type", "object_pk")
--- site/web/settings.py
+++ site/web/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "sass_processor",
     "accounts",
     "common",
+    "comments",
     "about",
     "quotes",
     "dashboard",
