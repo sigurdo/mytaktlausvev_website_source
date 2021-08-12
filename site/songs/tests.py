cbb90843de8e397b766196a05ce6edbb7116c79a
Shybert <25749775+Shybert@users.noreply.github.com>
2021-08-12 18:08:01 +0200

    Switched to factory-boy for Song tests

--- site/songs/tests.py
+++ site/songs/tests.py
@@ -1,16 +1,14 @@
 from http import HTTPStatus
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
+from common.factories import UserFactory
 from .models import Song
+from .factories import SongFactory
 
 
 class SongTestCase(TestCase):
     def setUp(self):
-        user = get_user_model().objects.create_user(username="bob", password="bob")
-        self.song = Song.objects.create(
-            title="Song of Tests", description="Test, test, test", created_by=user
-        )
+        self.song = SongFactory()
 
     def test_get_absolute_url(self):
         """Should have the correct absolute URL."""
@@ -21,34 +19,23 @@ class SongTestCase(TestCase):
 
 
 class SongCreateTestCase(TestCase):
-    def setUp(self):
-        self.user = get_user_model().objects.create_user(username="bob", password="bob")
-        self.client.login(username="bob", password="bob")
-
     def test_requires_login(self):
         """Should redirect to login page if user is not logged in."""
-        self.client.logout()
         response = self.client.get(reverse("song_create"))
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         self.assertTrue(response.url.startswith(reverse("login")))
 
     def test_author_set_to_current_user(self):
         """Should set the author to the current user on creation."""
+        self.user = UserFactory()
+        self.client.force_login(self.user)
         response = self.client.post(
             reverse("song_create"), {"title": "A Title", "description": "Some lyrics"}
         )
+
         self.assertEqual(Song.objects.count(), 1)
         song = Song.objects.last()
         self.assertEqual(song.created_by, self.user)
-
-    def test_redirects_to_created_song(self):
-        """Should redirect to the created song."""
-        response = self.client.post(
-            reverse("song_create"), {"title": "A Title", "description": "Some lyrics"}
-        )
-        self.assertRedirects(
-            response, reverse("song_detail", kwargs={"pk": Song.objects.last().pk})
-        )
 
 
 class SongUpdateTestCase(TestCase):
