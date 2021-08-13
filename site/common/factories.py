b8be1ba84084a4635ee6784d996ec9649dcd155b
Shybert <25749775+Shybert@users.noreply.github.com>
2021-08-13 08:12:46 +0200

    Added permission requirement for creating/updating Songs

--- site/common/factories.py
+++ site/common/factories.py
@@ -1,10 +1,35 @@
 import factory
 from django.conf import settings
+from django.contrib.auth.models import Permission
 
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = settings.AUTH_USER_MODEL
+        django_get_or_create = ("username",)
 
-    username = "username"
+    username = "user"
     password = "password"
+
+    @factory.post_generation
+    def permissions(self, create, permission_list):
+        if not create or not permission_list:
+            return
+
+        for permission in permission_list:
+            (app_label, codename) = permission.split(".")
+            self.user_permissions.add(
+                Permission.objects.get(
+                    content_type__app_label=app_label, codename=codename
+                )
+            )
+
+
+class SuperUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = settings.AUTH_USER_MODEL
+
+    username = "root"
+    password = "password"
+    is_staff = True
+    is_superuser = True
