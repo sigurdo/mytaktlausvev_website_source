34fc7157a68ab32df588b7bff776c80187028c03
Shybert <25749775+Shybert@users.noreply.github.com>
2021-08-24 19:54:24 +0200

    Removed `artiklar` prefixed `articles:detail` redirect
    Not really necessary, and overlaps with the other URLs.

--- site/articles/urls.py
+++ site/articles/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from django.views.generic.base import RedirectView
 from . import views
 
 app_name = "articles"
@@ -7,7 +6,6 @@ app_name = "articles"
 articles_patterns = [
     path("ny/", views.ArticleCreate.as_view(), name="create"),
     path("<slug:slug>/rediger/", views.ArticleUpdate.as_view(), name="update"),
-    path("<slug:slug>/", RedirectView.as_view(pattern_name="articles:detail")),
 ]
 urlpatterns = [
     path("<slug:slug>/", views.ArticleDetail.as_view(), name="detail"),
