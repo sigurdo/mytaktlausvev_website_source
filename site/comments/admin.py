5ed18d7202da5ed2de018e140dfafd010c43ba3d
Shybert <25749775+Shybert@users.noreply.github.com>
2021-10-22 19:53:38 +0200

    Improve comment admin with search and more fields

--- site/comments/admin.py
+++ site/comments/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Comment
 
-admin.site.register(Comment)
+
+class CommentAdminCustom(admin.ModelAdmin):
+    list_display = ("__str__", "created_by", "submitted")
+    search_fields = ("comment",)
+
+
+admin.site.register(Comment, CommentAdminCustom)
