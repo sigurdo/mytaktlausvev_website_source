7594f51c657b9b2963f8a975f993581260b0cd9e
Shybert <25749775+Shybert@users.noreply.github.com>
2021-11-05 09:39:03 +0100

    Register attendance inline in admin

--- site/events/admin.py
+++ site/events/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Event, EventAttendance
 
 
+class EventAttendanceInline(admin.TabularInline):
+    model = EventAttendance
+
+
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    inlines = (EventAttendanceInline,)
 
 
 admin.site.register(Event, EventAdmin)
