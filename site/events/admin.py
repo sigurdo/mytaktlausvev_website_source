67ef02a978601fa5e7a9602af0137ca259dcaf96
Shybert <25749775+Shybert@users.noreply.github.com>
2021-11-06 05:55:37 +0100

    Improve events admin

--- site/events/admin.py
+++ site/events/admin.py
@@ -9,7 +9,14 @@ class EventAttendanceInline(admin.TabularInline):
 class EventAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     inlines = (EventAttendanceInline,)
+    list_display = ("title", "start_time")
+    search_fields = ("title",)
+
+
+class EventAttendanceAdmin(admin.ModelAdmin):
+    list_display = ("event", "person", "status")
+    search_fields = ("event__title", "person__name")
 
 
 admin.site.register(Event, EventAdmin)
-admin.site.register(EventAttendance)
+admin.site.register(EventAttendance, EventAttendanceAdmin)
