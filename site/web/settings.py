5ab48ccff125a504638ede0b42715557ebfe2d30
Sigurd H Tullander <legotull.99@gmail.com>
2021-11-18 11:58:44 +0100

    Delete  duplicate MEDIA_ROOT definition

--- site/web/settings.py
+++ site/web/settings.py
@@ -183,9 +183,6 @@ CRISPY_FAIL_SILENTLY = not DEBUG
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-MEDIA_URL = "/media/"
-
 
 # Sass config
 
