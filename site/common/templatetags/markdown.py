726ac69a15c6ac8b4d84a207124f4bb75af21265
Sigurd H Tullander <legotull.99@gmail.com>
2021-12-01 15:50:58 +0100

    Add support for strikethough and underline in markdown filter

--- site/common/templatetags/markdown.py
+++ site/common/templatetags/markdown.py
@@ -1,10 +1,37 @@
 from django import template
 from django.utils.safestring import mark_safe
 import markdown as md
+from markdown.inlinepatterns import SimpleTagPattern
+from markdown.extensions import Extension
 import bleach
 from common.bootstrap_tables import BootstrapTableExtension
 
 register = template.Library()
+
+
+class StrikeThroughExtension(Extension):
+    """
+    Adds the possibility to use "~~something~~" to create a span that looks like <del>something</del>
+    """
+
+    RE = r"(~~)(.*?)~~"
+
+    def extendMarkdown(self, md):
+        del_tag = SimpleTagPattern(self.RE, "del")
+        md.inlinePatterns.add("del", del_tag, "_begin")
+
+
+class UnderlineExtension(Extension):
+    """
+    Adds the possibility to use "__something__" to create a span that looks like <ins>something</ins>
+    """
+
+    RE = r"(__)(.*?)__"
+
+    def extendMarkdown(self, md):
+        ins_tag = SimpleTagPattern(self.RE, "ins")
+        md.inlinePatterns.add("ins", ins_tag, ">del")
+
 
 ALLOWED_TAGS = [
     "h1",
@@ -47,6 +74,8 @@ ALLOWED_TAGS = [
     "acronym",
     "br",
     "cite",
+    "del",
+    "ins",
 ]
 
 ALLOWED_ATTRIBUTES = ["src", "alt", "width", "height", "class", "href"]
@@ -62,6 +91,8 @@ def markdown(string):
             "codehilite",
             "tables",
             BootstrapTableExtension(),
+            StrikeThroughExtension(),
+            UnderlineExtension(),
         ],
     )
     bleached = bleach.clean(converted, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)
