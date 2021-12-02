aad1f88579c4aa2bd22e7b15b873ce6f83555d57
Shybert <25749775+Shybert@users.noreply.github.com>
2021-12-02 11:03:09 +0100

    Move several `settings.py` values into .env file

--- site/web/settings.py
+++ site/web/settings.py
@@ -16,21 +16,23 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TESSDATA_DIR = os.path.join(BASE_DIR, "..", "tessdata", "tessdata_best-4.1.0")
 
-# Simplifies management stuff like deleting output files from the code editor on the host system.
-os.umask(0)
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ")c9b-kx9169s*!v1i^era6)ez3^k*io3c9#d+(*gf52-i0wh_0"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = [".localhost", "127.0.0.1", "[::1]", "0.0.0.0"]
+# ALLOWED_HOSTS should be a string of space-separated hosts
+# e.g. ALLOWED_HOSTS=localhost 127.0.0.1 [::1]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
+# Simplifies management stuff like deleting output files from the code editor on the host system.
+if DEBUG:
+    os.umask(0)
 
 # Application definition
 
@@ -102,12 +104,12 @@ WSGI_APPLICATION = "web.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "taktlaus_db",
-        "USER": "taktlaus",
-        "PASSWORD": "taktlaus",
-        "HOST": "db",
-        "PORT": 5432,
+        "ENGINE": os.environ.get("DB_ENGINE"),
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
