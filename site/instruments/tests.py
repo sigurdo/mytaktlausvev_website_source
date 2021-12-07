90740cadf70662baaf2b4de084031269c34c323e
Sigurd H Tullander <legotull.99@gmail.com>
2021-12-07 23:48:14 +0100

    Write some more instruments tests

--- site/instruments/tests.py
+++ site/instruments/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.db import IntegrityError
+from django.urls import reverse
 
 from common.mixins import TestMixin
 
-from .factories import InstrumentGroupFactory
+from .factories import InstrumentGroupFactory, InstrumentLocationFactory, InstrumentFactory
 
 
 class InstrumentGroupTestSuite(TestMixin, TestCase):
@@ -11,3 +13,23 @@ class InstrumentGroupTestSuite(TestMixin, TestCase):
 
     def test_to_str(self):
         self.assertEqual(str(self.instrument_group), "Tuba")
+
+
+class InstrumentLocationTestSuite(TestMixin, TestCase):
+    def setUp(self):
+        self.instrument_location = InstrumentLocationFactory(name="Lager")
+
+    def test_to_str(self):
+        self.assertEqual(str(self.instrument_location), "Lager")
+
+
+class InstrumentTestSuite(TestMixin, TestCase):
+    def setUp(self):
+        self.instrument = InstrumentFactory(name="Tuba 1")
+
+    def test_to_str(self):
+        self.assertEqual(str(self.instrument), "Tuba 1")
+    
+    def test_name_unique(self):
+        with self.assertRaises(IntegrityError):
+            InstrumentFactory(name="Tuba 1")
