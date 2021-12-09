705c7884ffaf685e47accd24e0b75ab42d076417
Sigurd H Tullander <legotull.99@gmail.com>
2021-12-09 02:14:59 +0100

    Begone django_pwa

--- site/common/context_processors.py
+++ site/common/context_processors.py
@@ -1,5 +1,13 @@
 from django.conf import settings
 
 
-def pwa_app_enabled(request):
-    return {"PWA_APP_ENABLED": settings.PWA_APP_ENABLED}
+def enable_pwa_manifest(request):
+    return {
+        "ENABLE_PWA_MANIFEST": settings.ENABLE_PWA_MANIFEST,
+    }
+
+
+def enable_serviceworker(request):
+    return {
+        "ENABLE_SERVICEWORKER": settings.ENABLE_SERVICEWORKER,
+    }
