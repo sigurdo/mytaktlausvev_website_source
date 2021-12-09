f981f14a614e9051447b93a68e6917676b7d45c1
Shybert <25749775+Shybert@users.noreply.github.com>
2021-12-09 02:49:07 +0100

    Rename `web` container to `django`

--- setup/init.sh
+++ setup/init.sh
@@ -3,8 +3,8 @@ set -e
 
 docker-compose build
 
-docker-compose run web ./site/manage.py migrate
-docker-compose run web ./site/manage.py create_dev_data
-docker-compose run web ./site/manage.py generate_code_styles site/static/scss default monokai
+docker-compose run django ./site/manage.py migrate
+docker-compose run django ./site/manage.py create_dev_data
+docker-compose run django ./site/manage.py generate_code_styles site/static/scss default monokai
 
 docker-compose down
