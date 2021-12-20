90aa5b5bdd0a5a49e72da95ab67b6beb8da4b0fc
Sigurd H Tullander <legotull.99@gmail.com>
2021-12-20 23:26:54 +0100

    Make ical show button more robust

--- site/events/static/events/event_list.js
+++ site/events/static/events/event_list.js
@@ -5,10 +5,15 @@ document.querySelector("#ical-link-collapse-button").addEventListener("click", (
     });
     let ical_link_a = document.querySelector("#ical-link");
     let feedback_span = document.querySelector("#ical-link-collapse-clipboard-feedback");
-    navigator.clipboard.writeText(ical_link_a.href).then(() => {
-        feedback_span.innerHTML = "vart kopiert til utklippstavla di."
-    }, () => {
+    try {
+        navigator.clipboard.writeText(ical_link_a.href).then(() => {
+            feedback_span.innerHTML = "vart kopiert til utklippstavla di."
+        }, () => {
+            throw Error()
+        });   
+    }
+    catch (err) {
         feedback_span.innerHTML = "vart dessverre ikkje kopiert automatisk til utklippstavla di, men det klarar du sikkert på eiga hand."
-    });
+    }
     ical_link_collapse.show();
 });
