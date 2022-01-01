f89d4f35689d68135411189629dcaa3c167167c6
Shybert <25749775+Shybert@users.noreply.github.com>
2022-01-01 13:27:38 +0100

    Generic view for creating a model with an inline formset

--- site/common/views.py
+++ site/common/views.py
@@ -2,7 +2,81 @@ from typing import Any, Dict
 
 from django import http
 from django.core import exceptions
-from django.views.generic.edit import UpdateView
+from django.db import transaction
+from django.http.response import HttpResponseRedirect
+from django.views.generic.edit import CreateView, UpdateView
+
+
+class InlineFormsetCreateView(CreateView):
+    """
+    View for creating a model and multiple related models with an inline formset.
+
+    Inherits methods and attributes from `CreateView`
+    Implementors must also define the `formset_class` attribute.
+    """
+
+    def get_context_data(self, **kwargs):
+        if "formset" not in kwargs:
+            kwargs["formset"] = self.get_formset()
+        return super().get_context_data(**kwargs)
+
+    def get_formset_class(self):
+        """Return the formset class to use in the view."""
+        return self.formset_class
+
+    def get_formset_kwargs(self):
+        """Return the keyword arguments for instantiating the formset."""
+        kwargs = super().get_form_kwargs()
+        if hasattr(self, "object"):
+            kwargs.update({"instance": self.object})
+        if self.request.method in ("POST", "PUT"):
+            kwargs.update(
+                {
+                    "data": self.request.POST,
+                    "files": self.request.FILES,
+                }
+            )
+        return kwargs
+
+    def get_formset(self, formset_class=None):
+        """Return an instance of the formset to be used in this view."""
+        if formset_class is None:
+            formset_class = self.get_formset_class()
+        return formset_class(**self.get_formset_kwargs())
+
+    def formset_valid(self, formset):
+        """If the formset is valid, save the associated models."""
+        formset.instance = self.object
+        formset.save()
+
+    def formset_invalid(self, formset):
+        """If the formset is invalid, render the invalid formset."""
+        return self.render_to_response(self.get_context_data(formset=formset))
+
+    def form_and_formset_valid(self):
+        """
+        If both the form and the formset is valid,
+        redirect to the supplied URL
+        """
+        return HttpResponseRedirect(self.get_success_url())
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        with transaction.atomic():
+            form = self.get_form()
+            if form.is_valid():
+                self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+
+            formset = self.get_formset()
+            if formset.is_valid():
+                self.formset_valid(formset)
+            else:
+                transaction.set_rollback(True)
+                return self.formset_invalid(formset)
+
+        return self.form_and_formset_valid()
 
 
 class FormAndFormsetUpdateView(UpdateView):
