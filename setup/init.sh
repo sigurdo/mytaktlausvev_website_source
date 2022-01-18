e59018f8ba4f03ef1b5a738fd5a905c5d68d15c8
Shybert <25749775+Shybert@users.noreply.github.com>
2022-01-18 15:14:40 +0100

    Stop and remove container and images in init script

--- setup/init.sh
+++ setup/init.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+docker-compose down -v
+
 docker-compose build
 docker-compose up -d
 
+docker-compose exec -T django ./site/manage.py generate_code_styles site/static/scss default monokai
 docker-compose exec -T django ./site/manage.py migrate
 docker-compose exec -T django ./site/manage.py create_dev_data
-docker-compose exec -T django ./site/manage.py generate_code_styles site/static/scss default monokai
 
 docker-compose down
