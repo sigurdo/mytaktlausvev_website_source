bc6aa4574c669b7af92d7d3988c67f54d1640e67
Ingvild <ingvlt@stud.ntnu.no>
2022-01-24 18:22:26 +0100

    fix accounts/models and accounts/urls rebase fix

--- site/accounts/urls.py
+++ site/accounts/urls.py
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-from django.urls import path
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
-from .views import ProfileDetail, UserCustomCreate
+from .views import ProfileDetail, MemberList, UserCustomCreate
 
 app_name = "accounts"
 
 urlpatterns = [
     path("ny/", UserCustomCreate.as_view(), name="UserCustomCreate"),
     path("profil/<slug:slug>/", ProfileDetail.as_view(), name="ProfileDetail"),
-=======
-from django.urls import include, path
-from django.contrib.auth import views as auth_views
-from .forms import LoginForm
-from .views import ProfileDetail, MemberList
-
-urlpatterns = [
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="accounts/login.html", authentication_form=LoginForm
-        ),
-        name="login",
-    ),
-    path("profil/<slug:slug>/", ProfileDetail.as_view(), name="profile"),
-    path("", include("django.contrib.auth.urls")),
     path("", MemberList.as_view(), name="medlemmer"),
 
->>>>>>> start medlemsliste
 ]
