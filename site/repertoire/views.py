468bfd8af58fb278e0a2ffba326de647c4cb7729
Sigurd Tullander <legotull.99@gmail.com>
2022-01-25 14:40:50 +0100

    Implement RepertoirePdf.form_valid

--- site/repertoire/views.py
+++ site/repertoire/views.py
@@ -1,6 +1,11 @@
+from io import BytesIO
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import FileResponse
 from django.urls import reverse_lazy
+from django.utils.text import slugify
 from django.views.generic import DetailView, FormView, ListView
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 from common.views import (
     DeleteViewCustom,
@@ -62,29 +67,24 @@ class RepertoirePdf(LoginRequiredMixin, FormView, DetailView):
         initial = self.get_initial()
         for i, form in enumerate(formset.forms):
             score = initial[i]["score"]
-            # print("form:", form)
-            # print("fields:", form.fields)
-            # print("fields:", form.__dict__)
-            # # print("part:", form.fields["part"])
-            # print("score:", form.fields["score"])
-            # print("score:", form.fields["score"].__dict__)
-            # print("initial:", initial[i]["score"])
             form.fields["part"].queryset = Part.objects.filter(pdf__score=score)
-            # print("sånn")
-            # form.fields["part"].queryset = self.get_object().pdfs
         return formset
+
+    def form_valid(self, formset):
+        pdf_writer = PdfFileWriter()
+        for form in formset:
+            part = form.cleaned_data["part"]
+            pdf_writer.appendPagesFromReader(PdfFileReader(part.pdf_file()))
+        output_stream = BytesIO()
+        pdf_writer.write(output_stream)
+        output_stream.seek(0)
+        filename = slugify(f"{self.get_object()} {self.request.user}") + ".pdf"
+        return FileResponse(
+            output_stream,
+            content_type="application/pdf",
+            filename=filename,
+        )
 
     def get_context_data(self, **kwargs):
         kwargs["form_title"] = f"Generer PDF for {self.get_object()}"
         return super().get_context_data(**kwargs)
-
-    # content_type = "application/pdf"
-
-    # def render_to_response(self, _):
-    #     pdf_stream = self.get_object().favorite_parts_pdf_file(self.request.user)
-    #     filename = self.get_object().favorite_parts_pdf_filename(self.request.user)
-    #     return FileResponse(
-    #         pdf_stream,
-    #         content_type=self.content_type,
-    #         filename=filename,
-    #     )
