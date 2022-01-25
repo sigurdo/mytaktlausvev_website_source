76c509b806dc28df09bff5d0ed5295acacb34f1b
Overlintkonstabel Lintbot <lintbot@lint.police>
2022-01-25 11:48:14 +0000

    Lint

--- site/accounts/views.py
+++ site/accounts/views.py
@@ -40,6 +40,8 @@ class ProfileDetail(LoginRequiredMixin, DetailView):
     model = UserCustom
     template_name = "accounts/profile_detail.html"
     context_object_name = "profile"
+
+
 class MemberList(LoginRequiredMixin, ListView):
 
     model = UserCustom
