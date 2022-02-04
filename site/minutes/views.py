f590f7ca62a3fd21bf9c534615d51b184713e2c0
Shybert <25749775+Shybert@users.noreply.github.com>
2022-02-04 09:01:06 +0100

    Breadcrumbs for minutes views

--- site/minutes/views.py
+++ site/minutes/views.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
-from common.mixins import PermissionOrCreatedMixin
+from common.mixins import BreadcrumbsMixin, PermissionOrCreatedMixin
 
 from .forms import MinutesForm
 from .models import Minutes
+
+
+def breadcrumbs(minutes=None):
+    """Returns breadcrumbs for the minutes views."""
+    breadcrumbs = [{"url": reverse("minutes:MinutesList"), "name": "Alle referat"}]
+    if minutes:
+        breadcrumbs.append({"url": minutes.get_absolute_url(), "name": minutes})
+    return breadcrumbs
 
 
 class MinutesList(LoginRequiredMixin, ListView):
@@ -13,14 +22,20 @@ class MinutesList(LoginRequiredMixin, ListView):
     paginate_by = 50
 
 
-class MinutesDetail(LoginRequiredMixin, DetailView):
+class MinutesDetail(LoginRequiredMixin, BreadcrumbsMixin, DetailView):
     model = Minutes
 
+    def get_breadcrumbs(self):
+        return breadcrumbs()
 
-class MinutesCreate(LoginRequiredMixin, CreateView):
+
+class MinutesCreate(LoginRequiredMixin, BreadcrumbsMixin, CreateView):
     model = Minutes
     form_class = MinutesForm
     template_name = "common/form.html"
+
+    def get_breadcrumbs(self):
+        return breadcrumbs()
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
@@ -28,11 +43,14 @@ class MinutesCreate(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class MinutesUpdate(PermissionOrCreatedMixin, UpdateView):
+class MinutesUpdate(PermissionOrCreatedMixin, BreadcrumbsMixin, UpdateView):
     model = Minutes
     form_class = MinutesForm
     template_name = "common/form.html"
     permission_required = "minutes.change_minutes"
+
+    def get_breadcrumbs(self):
+        return breadcrumbs(self.object)
 
     def form_valid(self, form):
         form.instance.modified_by = self.request.user
