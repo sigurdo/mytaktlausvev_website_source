a7d62797cc3398d253aca4283df17906cccf2b8f
Sigurd Tullander <legotull.99@gmail.com>
2022-02-08 02:51:23 +0100

    Finish and clean up dashboard

--- site/dashboard/tests.py
+++ site/dashboard/tests.py
@@ -1,5 +1,96 @@
-"""Tests for the 'about'-app"""
+from datetime import datetime, timedelta
 
-# from django.test import TestCase
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import make_aware
 
-# Create your tests here.
+from accounts.factories import UserFactory
+from articles.factories import ArticleFactory
+from comments.factories import CommentFactory
+from common.mixins import TestMixin
+from events.factories import EventFactory
+from minutes.factories import MinutesFactory
+from pictures.factories import GalleryFactory, ImageFactory
+
+
+class DashboardRedirectTestSuite(TestMixin, TestCase):
+    def get_url(self):
+        return reverse("dashboard:DashboardRedirect")
+
+    def test_not_logged_in(self):
+        ArticleFactory(slug="om-oss", public=True)
+        response = self.client.get(self.get_url())
+        self.assertRedirects(
+            response, reverse("articles:ArticleDetail", args=["om-oss"])
+        )
+
+    def test_logged_in(self):
+        self.client.force_login(UserFactory())
+        response = self.client.get(self.get_url())
+        self.assertRedirects(response, reverse("dashboard:Dashboard"))
+
+
+class DashboardTestSuite(TestMixin, TestCase):
+    def get_url(self):
+        return reverse("dashboard:Dashboard")
+
+    def test_requires_login(self):
+        self.assertLoginRequired(self.get_url())
+
+    def get_context(self):
+        self.client.force_login(UserFactory())
+        return self.client.get(self.get_url()).context
+
+    def test_quotes_in_context(self):
+        context = self.get_context()
+        self.assertIn("quotes", context)
+
+    def test_events_in_context(self):
+        EventFactory()
+        context = self.get_context()
+        self.assertIn("events", context)
+        self.assertEquals(len(list(context["events"])), 1)
+
+    def test_minutes_in_context(self):
+        MinutesFactory()
+        context = self.get_context()
+        self.assertIn("minutes", context)
+        self.assertEquals(len(list(context["minutes"])), 1)
+
+    def test_latest_galleries_in_context(self):
+        ImageFactory()
+        context = self.get_context()
+        self.assertIn("latest_galleries", context)
+        self.assertEquals(len(list(context["latest_galleries"])), 1)
+
+    def test_latest_galleries_excludes_empty(self):
+        GalleryFactory()
+        context = self.get_context()
+        self.assertEquals(len(list(context["latest_galleries"])), 0)
+
+    def test_random_images_in_context(self):
+        context = self.get_context()
+        self.assertIn("random_images", context)
+
+    def test_random_images_does_not_include_new_image(self):
+        ImageFactory()
+        context = self.get_context()
+        self.assertEquals(len(list(context["random_images"])), 0)
+
+    def test_random_images_includes_3_year_old_image(self):
+        image = ImageFactory()
+        image.uploaded = make_aware(datetime.now() - timedelta(days=3 * 365))
+        image.save()
+        context = self.get_context()
+        self.assertEquals(len(list(context["random_images"])), 1)
+
+    def test_random_images_does_not_includes_10_year_old_image(self):
+        ImageFactory(uploaded=make_aware(datetime.now() - timedelta(days=10 * 365)))
+        context = self.get_context()
+        self.assertEquals(len(list(context["random_images"])), 0)
+
+    def test_latest_comments_in_context(self):
+        CommentFactory(content_object=ArticleFactory())
+        context = self.get_context()
+        self.assertIn("latest_comments", context)
+        self.assertEquals(len(list(context["latest_comments"])), 1)
--- site/dashboard/views.py
+++ site/dashboard/views.py
@@ -1,22 +1,70 @@
 """Views for the 'dashboard'-module."""
 from datetime import datetime, timedelta
 
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Max
+from django.urls import reverse
 from django.utils.timezone import make_aware
+from django.views.generic import RedirectView, TemplateView
 
+from comments.models import Comment
+from common.utils import random_sample_queryset
 from events.models import Event
+from minutes.models import Minutes
+from pictures.models import Gallery, Image
 from quotes.models import Quote
 
 
-def dashboard(request):
-    """View for the main dashboard."""
-    return render(
-        request,
-        "dashboard/dashboard.html",
-        {
-            "quotes": Quote.objects.all()[:2],
-            "events": Event.objects.filter(
-                start_time__gte=make_aware(datetime.now() - timedelta(1))
-            )[:4],
-        },
-    )
+class DashboardRedirect(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return reverse("dashboard:Dashboard")
+        return reverse("articles:ArticleDetail", args=["om-oss"])
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = "dashboard/dashboard.html"
+
+    def get_quotes(self):
+        """Returns 2 random quotes."""
+        return random_sample_queryset(Quote.objects.all(), 2)
+
+    def get_events(self):
+        """Returns the 4 closest upcoming events."""
+        return Event.objects.filter(
+            start_time__gte=make_aware(datetime.now() - timedelta(1))
+        )[:4]
+
+    def get_minutes(self):
+        """Returns the 3 most recent minutes."""
+        return Minutes.objects.all()[:3]
+
+    def get_latest_galleries(self):
+        """Returns the 2 galleries with most recent image uploads."""
+        return (
+            Gallery.objects.all()
+            .exclude(images__isnull=True)
+            .alias(latest_upload=Max("images__uploaded"))
+            .order_by("-latest_upload")[:2]
+        )
+
+    def get_random_images(self):
+        """Returns 2 random images."""
+        queryset = Image.objects.filter(
+            uploaded__lte=make_aware(datetime.now() - timedelta(days=1 * 365)),
+            uploaded__gte=make_aware(datetime.now() - timedelta(days=5 * 365)),
+        )
+        return random_sample_queryset(queryset, 2)
+
+    def get_latest_comments(self):
+        """Returns 5 most recent comments."""
+        return Comment.objects.all().order_by("-created")[:5]
+
+    def get_context_data(self, **kwargs):
+        kwargs["quotes"] = self.get_quotes()
+        kwargs["events"] = self.get_events()
+        kwargs["minutes"] = self.get_minutes()
+        kwargs["latest_galleries"] = self.get_latest_galleries()
+        kwargs["random_images"] = self.get_random_images()
+        kwargs["latest_comments"] = self.get_latest_comments()
+        return super().get_context_data(**kwargs)
