d7e8d734fd677d40b54b6b9cdca65a06934c9aa3
Sigurd Tullander <legotull.99@gmail.com>
2022-02-10 19:46:05 +0100

    Make forum posts look slightly cleaner by labelling as innlegg instead of kommentar

--- site/comments/templatetags/comments.py
+++ site/comments/templatetags/comments.py
@@ -8,14 +8,19 @@ register = template.Library()
 
 
 @register.inclusion_tag("comments/comment_list.html", takes_context=True)
-def comment_list(context, model):
+def comment_list(context, model, forum_posts=False):
     content_object_data = {
         "content_type": ContentType.objects.get_for_model(model),
         "object_pk": model.pk,
     }
+    form = CommentCreateForm(initial=content_object_data)
+    if forum_posts:
+        form.fields["comment"].label = "Innlegg"
+        form.helper.inputs[0].value = "Legg ut innlegg"
     return {
         "comments": Comment.objects.filter(**content_object_data),
-        "form": CommentCreateForm(initial=content_object_data),
+        "form": form,
         "user": context.request.user,
         "perms": context.get("perms"),
+        "forum_posts": forum_posts,
     }
