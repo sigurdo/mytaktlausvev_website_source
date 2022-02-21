78e69ef6c750580b0543ce6bd708d5380fb3bb98
Overlintkonstabel Lintbot <lintbot@lint.police>
2022-02-21 22:58:15 +0000

    Lint

--- site/storage/tests.py
+++ site/storage/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 from common.mixins import TestMixin
 
+
 class StorageAccessViewTestSuite(TestMixin, TestCase):
     def get_url(self):
         return reverse("storage:StorageAccessView")
@@ -10,7 +11,7 @@ class StorageAccessViewTestSuite(TestMixin, TestCase):
     def test_requires_login(self):
         """Should require login."""
         self.assertLoginRequired(self.get_url())
-    
+
     def test_requires_permission(self):
         self.assertPermissionRequired(
             self.get_url(),
