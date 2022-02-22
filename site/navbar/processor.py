2808735a919c13634e9ff3b6854bf1dd3ab01e40
Audun <audun.asdal@gmail.com>
2022-02-22 19:32:13 +0100

    added green navbar when DEBUG=1

---
+++ site/navbar/processor.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+
+def debug_flag(request):
+    debug_flag = settings.DEBUG
+    return {'debug_flag':debug_flag}
--- site/web/settings.py
+++ site/web/settings.py
@@ -125,6 +125,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "common.context_processors.enable_pwa_manifest",
                 "common.context_processors.enable_serviceworker",
+                "navbar.processor.debug_flag",
             ],
         },
     },
