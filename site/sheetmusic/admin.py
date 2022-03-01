a1f61e8960f0ff98865de31e7615bac11f33b65e
Shybert <25749775+Shybert@users.noreply.github.com>
2022-03-01 20:12:07 +0100

    Sheetmusic admin PR suggestions by @sigurdo

--- site/sheetmusic/admin.py
+++ site/sheetmusic/admin.py
@@ -1,28 +1,43 @@
 """Admin settings for the 'sheetmusic'-app"""
 
-from django.contrib.admin import ModelAdmin, display, site
+from django.contrib.admin import ModelAdmin, TabularInline, display, site
 
 from .models import Part, Pdf, Score
+
+
+class PdfInline(TabularInline):
+    model = Pdf
 
 
 class ScoreAdmin(ModelAdmin):
     list_display = ("title", "arrangement", "originally_from", "created")
     search_fields = ("title", "arrangement", "originally_from")
     prepopulated_fields = {"slug": ("title",)}
+    inlines = (PdfInline,)
+
+
+class PartInline(TabularInline):
+    model = Part
 
 
 class PdfAdmin(ModelAdmin):
     list_display = ("filename_original", "score")
     search_fields = ("filename_original", "score__title")
+    inlines = (PartInline,)
 
 
 class PartAdmin(ModelAdmin):
-    list_display = ("__str__", "score_title")
-    search_fields = ("instrument_type__name", "part_number", "note")
+    list_display = ("__str__", "score")
+    search_fields = (
+        "instrument_type__name",
+        "part_number",
+        "note",
+        "pdf__score__title",
+    )
 
     @display(description="Note")
-    def score_title(self, obj):
-        return obj.pdf.score.title
+    def score(self, obj):
+        return obj.pdf.score
 
 
 site.register(Score, ScoreAdmin)
