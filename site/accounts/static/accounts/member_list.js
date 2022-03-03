bb06f17996a3311f6fb2e1e60f07fcf2779075b6
Sigurd Tullander <legotull.99@gmail.com>
2022-03-03 02:04:00 +0100

    Filter memberlist on membership status ++

--- site/accounts/static/accounts/member_list.js
+++ site/accounts/static/accounts/member_list.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new DataTable('#member_overview', {
+    let data_table = new DataTable('#member_overview', {
         "order": [[2, "asc"]],
         "paging": false,
         "language": {
             "zeroRecords": "Ingen medlemar funne",
             // Would otherwise say "showing page 1 of 1" even though we've turned off paging
-            "info": "",
+            "info": "Visar _TOTAL_ medlemmar",
             "infoEmpty": "Ingen  medlemar funne",
             "infoFiltered": "(filtrert fra totalt _MAX_ member)",
             "search": "Søk:",
         }
+    });
+
+    // Filter on membership status when that select gets an input
+    document.querySelector("#select-membership-status").addEventListener("input", event => {
+        let value = $.fn.dataTable.util.escapeRegex(event.target.value);
+        data_table.column(2).search( value ? '^'+value+'$' : '', true, false ).draw();
     });
 } );
