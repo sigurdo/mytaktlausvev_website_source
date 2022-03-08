fa581775598f6ea8ecdafae80e0ec37553214ee6
Marius Vik <marius.vik01@gmail.com>
2022-03-08 22:06:13 +0100

    random algorithm added for easter egg

--- site/quotes/views.py
+++ site/quotes/views.py
@@ -9,6 +9,8 @@ from quotes.models import Quote
 
 from .forms import QuoteForm
 
+import random
+
 
 class QuoteNew(LoginRequiredMixin, CreateView):
     """View-function for new-quote-form"""
@@ -29,9 +31,18 @@ class QuoteNew(LoginRequiredMixin, CreateView):
 class QuoteList(LoginRequiredMixin, ListView):
     """View-function for displaying all quotes"""
 
+
     model = Quote
     context_object_name = "quotes"
     paginate_by = 50
+
+    def get_template_names(self):
+        random_number = random.randint(50,150)
+        random_number2 = random.randint(1,random_number)
+        random_number3 = random.randint(1, random_number)
+        if (random_number2==random_number3):
+            return "quotes/quote_list_shame.html"
+        return super().get_template_names()
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
