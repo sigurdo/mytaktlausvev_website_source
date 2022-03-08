0ff5be158fbdc159cd3262feab9c41d2c2d0f404
Shybert <25749775+Shybert@users.noreply.github.com>
2022-03-08 21:02:38 +0100

    Member list changes
    * Explain the new membership statuses
    * Styled membership status select
    * Order by membership status by default, then name
    * Ordering by membership status also orders by name

--- site/accounts/static/accounts/member_list.js
+++ site/accounts/static/accounts/member_list.js
@@ -1,19 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let data_table = new DataTable('#member_overview', {
-        "order": [[2, "asc"]],
-        "paging": false,
-        "language": {
-            "zeroRecords": "Ingen medlemmar funne",
-            "info": "Visar _TOTAL_ medlemmar",
-            "infoEmpty": "Ingen  medlemmar funne",
-            "infoFiltered": "(filtrert fra totalt _MAX_ medlemmar)",
-            "search": "Søk:",
-        }
-    });
+document.addEventListener("DOMContentLoaded", () => {
+  let data_table = new DataTable("#member_overview", {
+    order: [
+      [2, "asc"],
+      [1, "asc"],
+    ],
+    paging: false,
+    language: {
+      zeroRecords: "Ingen medlemmar funne",
+      info: "Visar _TOTAL_ medlemmar",
+      infoEmpty: "Ingen  medlemmar funne",
+      infoFiltered: "(filtrert fra totalt _MAX_ medlemmar)",
+      search: "Søk:",
+    },
+    columns: [
+      { orderable: false },
+      null,
+      { orderData: [2, 1] },
+      null,
+      { orderable: false },
+      { orderable: false },
+    ],
+  });
 
-    // Filter on membership status when that select gets an input
-    document.querySelector("#select-membership-status").addEventListener("input", event => {
-        let value = $.fn.dataTable.util.escapeRegex(event.target.value);
-        data_table.column(2).search( value ? '^'+value+'$' : '', true, false ).draw();
+  // Filter on membership status when that select gets an input
+  document
+    .querySelector("#select-membership-status")
+    .addEventListener("input", (event) => {
+      data_table
+        .column(2)
+        .search(
+          event.target.value ? "^" + event.target.value + "$" : "",
+          true,
+          false
+        )
+        .draw();
     });
-} );
+});
