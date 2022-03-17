1d94fb7e18f5a633300e11c517837984a61e1001
Shybert <25749775+Shybert@users.noreply.github.com>
2022-03-17 02:29:12 +0100

    Revert "Nuke light mode"
    This reverts commit 66d19cd02b44c7351d35ff7e74a231a7164aa12e.

--- setup/init.sh
+++ setup/init.sh
@@ -6,7 +6,7 @@ docker-compose down -v
 docker-compose build
 docker-compose up -d
 
-docker-compose exec -T django ./site/manage.py generate_code_styles site/static/scss monokai
+docker-compose exec -T django ./site/manage.py generate_code_styles site/static/scss default monokai
 docker-compose exec -T django ./site/manage.py migrate
 docker-compose exec -T django ./site/manage.py create_dev_data
 
