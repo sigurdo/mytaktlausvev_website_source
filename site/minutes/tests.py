e29e5c852f005483fe22a768271a67cc8e56daa1
Shybert <25749775+Shybert@users.noreply.github.com>
2022-04-13 23:07:33 +0200

    Break import sort order

--- site/minutes/tests.py
+++ site/minutes/tests.py
@@ -7,8 +7,8 @@ from django.utils.text import slugify
 
 from accounts.factories import SuperUserFactory
 from common.mixins import TestMixin
-from minutes.factories import MinutesFactory
 from minutes.models import Minutes
+from minutes.factories import MinutesFactory
 
 
 class MinutesTestSuite(TestCase):
