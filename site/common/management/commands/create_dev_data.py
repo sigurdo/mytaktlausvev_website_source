043f3e2755e810919c380d623cfffa21993bf613
Shybert <25749775+Shybert@users.noreply.github.com>
2022-08-17 08:06:17 +0200

    Accessible button for downloading button designs

--- site/common/management/commands/create_dev_data.py
+++ site/common/management/commands/create_dev_data.py
@@ -10,6 +10,7 @@ from django.utils.timezone import make_aware
 from accounts.models import UserCustom
 from advent_calendar.factories import AdventCalendarFactory, WindowFactory
 from articles.factories import ArticleFactory
+from buttons.factories import ButtonDesignFactory
 from common.comments.factories import CommentFactory
 from common.embeddable_text.factories import EmbeddableTextFactory
 from common.test_utils import test_pdf_multipage
@@ -395,6 +396,8 @@ class Command(BaseCommand):
             created_by=leader,
             modified_by=leader,
         )
+
+        ButtonDesignFactory()
 
         group = Group.objects.create(name="Vevkom")
         leader.groups.add(group)
