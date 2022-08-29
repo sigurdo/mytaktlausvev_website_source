8d08f724203992e45ba75f6b0234c51f2fb1549d
Sigrid <sigridgr@ntebb.no>
2022-08-29 19:16:46 +0200

    Fix startapp script

--- scripts/startapp.sh
+++ scripts/startapp.sh
@@ -3,4 +3,4 @@ set -e               # Exit on error
 cd $(dirname $0)/../ # Set working directory to project root
 
 docker-compose run --rm --service-ports --workdir /app/site/ django python manage.py startapp $1
-sudo chown $USER:$USER site/$1
+sudo chown -R $USER:$USER site/$1
