21d483df068846672c51937d18ef146f59c5767c
Sigurd Tullander <legotull.99@gmail.com>
2022-09-05 14:30:57 +0200

    [#226] Show calendar feed start date on profile

--- site/accounts/models.py
+++ site/accounts/models.py
@@ -188,4 +188,8 @@ class UserCustom(AbstractUser):
             ("view_storage_access", "Kan sjå lagertilgjenge"),
             ("edit_storage_access", "Kan redigere lagertilgjenge"),
             ("view_image_sharing_consent", "Kan sjå samtykkje til deling av bilete"),
+            (
+                "view_calendar_feed_start_date",
+                "Kan sjå startdato for kalenderintegrasjon",
+            ),
         )
