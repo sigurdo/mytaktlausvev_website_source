332c929b13644fe2532d14932a14301e97a27f92
Shybert <25749775+Shybert@users.noreply.github.com>
2022-10-11 10:00:16 +0200

    Optimize checking if user has favorite parts for a score in `ScoreList`

--- site/web/settings.py
+++ site/web/settings.py
@@ -107,6 +107,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -114,7 +115,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "watson.middleware.SearchContextMiddleware",
     "django_userforeignkey.middleware.UserForeignKeyMiddleware",
     "common.middleware.RedirectFallbackMiddleware",
