636b6c0ca97241f6bb0ceec6d4830ea2502c5285
Sigrid <sigridgr@ntebb.no>
2022-10-19 14:19:24 +0200

    Allow users to edit events from gallery

--- site/pictures/forms.py
+++ site/pictures/forms.py
@@ -1,11 +1,16 @@
+from tkinter import Widget
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from django.forms import ClearableFileInput, DateInput, ModelForm
+from django.forms import ClearableFileInput, DateInput, ModelForm, ModelMultipleChoiceField
 from django.forms.models import inlineformset_factory
 
 from common.forms.mixins import CleanAllFilesMixin
 
 from .models import Gallery, Image
+
+from common.forms.widgets import AutocompleteSelectMultiple
+from events.models import Event
+
 
 
 class GalleryForm(ModelForm):
@@ -14,13 +19,38 @@ class GalleryForm(ModelForm):
     helper = FormHelper()
     helper.add_input(Submit("submit", "Lagre galleri"))
 
+    connected_events = ModelMultipleChoiceField(
+        queryset=Event.objects.order_by("-start_time"), required=False, widget=AutocompleteSelectMultiple, 
+    )
+
+
     class Meta:
         model = Gallery
-        fields = ["title", "date", "date_to", "content"]
+        fields = ["title", "date", "date_to", "content", "connected_events"]
         widgets = {
             "date": DateInput(attrs={"type": "date"}),
             "date_to": DateInput(attrs={"type": "date"}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.id and self.instance.connected_events:
+            self.initial['connected_events'] = self.instance.connected_events.all()
+    
+    def save(self):
+        gallery = super().save(commit=False)
+        gallery.save()
+        events = self.cleaned_data['connected_events']
+        old_events = Event.objects.filter(connected_gallery = gallery)
+        for event in events:
+            event.connected_gallery = gallery
+            event.save()
+        for old_event in old_events:
+            if old_event not in events:
+                old_event.connected_gallery = None
+                old_event.save()
+        return gallery
+
 
 
 class ImageCreateForm(CleanAllFilesMixin, ModelForm):
