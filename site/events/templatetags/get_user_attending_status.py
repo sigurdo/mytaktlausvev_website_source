5a0144d6ac24133b2370e0ef75a8ed2bfc64c2ac
Sigrid <sigridgr@ntebb.no>
2022-10-21 00:41:44 +0200

    Make status readable

---
+++ site/events/templatetags/get_user_attending_status.py
@@ -0,0 +1,16 @@
+from django import template
+
+from events.models import Attendance
+
+register = template.Library()
+
+
+@register.simple_tag
+def translate_attending_status(status):
+    if status==Attendance.ATTENDING:
+        return Attendance.ATTENDING.label
+    elif status==Attendance.ATTENDING_MAYBE:
+        return Attendance.ATTENDING_MAYBE.label
+    elif status==Attendance.ATTENDING_NOT:
+        return Attendance.ATTENDING_NOT.label
+    return "Du har ikkje svart"
