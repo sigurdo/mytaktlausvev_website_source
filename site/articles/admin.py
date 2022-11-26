21ecb5e226ca89f8574d5fc6f400196766567bd9
Shybert <25749775+Shybert@users.noreply.github.com>
2022-11-26 08:04:24 +0100

    Improve `Article` admin

--- site/articles/admin.py
+++ site/articles/admin.py
@@ -4,6 +4,17 @@ from .models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "parent",
+        "public",
+        "comments_allowed",
+        "created_by",
+        "created",
+    )
+    search_fields = ("title", "parent__title", "created_by__name")
+    list_filter = ("public", "comments_allowed")
+    readonly_fields = ("created", "created_by", "modified", "modified_by")
     prepopulated_fields = {"slug": ("title",)}
 
 
