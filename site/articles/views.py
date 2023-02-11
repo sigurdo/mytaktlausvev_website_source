e657cb55729dffce597259487c484a4c5c1cfed4
Sigurd Tullander <legotull.99@gmail.com>
2023-02-11 17:56:40 +0100

    Rewrite article breadcrumbs

--- site/articles/views.py
+++ site/articles/views.py
@@ -18,20 +18,27 @@ class SlugPathMixin:
     Raises a 404 if a matching object can't be found.
     Intented to override `SingleObjectMixin`.
     """
+    # Cannot be called `object`, because that will make `SubarticleCreate`
+    # load `ArticleForm` as an update form instead of a create form.
+    object_cached = None
 
     def get_object(self):
+        if self.object_cached is not None:
+            return self.object_cached
+
         path = self.kwargs.get("path")
         slug_object = path.split("/")[-1]
 
         candidates = self.get_queryset().filter(slug=slug_object)
         for candidate in candidates:
             if candidate.path() == path:
-                return candidate
+                self.object_cached = candidate
+                return self.object_cached
 
         raise Http404(f"Couldn't find and article matching path '{path}'.")
 
 
-class ArticleDetail(UserPassesTestMixin, BreadcrumbsMixin, SlugPathMixin, DetailView):
+class ArticleDetail(UserPassesTestMixin, SlugPathMixin, BreadcrumbsMixin, DetailView):
     """View for viewing an article."""
 
     model = Article
@@ -39,7 +46,7 @@ class ArticleDetail(UserPassesTestMixin, BreadcrumbsMixin, SlugPathMixin, Detail
     def test_func(self):
         return self.get_object().public or self.request.user.is_authenticated
 
-    def get_breadcrumbs(self) -> list:
+    def get_breadcrumbs(self):
         return self.object.breadcrumbs()
 
     def get_context_data(self, **kwargs):
@@ -49,11 +56,14 @@ class ArticleDetail(UserPassesTestMixin, BreadcrumbsMixin, SlugPathMixin, Detail
         else:
             context["subarticles"] = self.object.children.filter(public=True)
         return context
+    
+    @classmethod
+    def get_breadcrumbs_for_children(cls, article, **kwargs):
+        return article.breadcrumbs(include_self=True)
 
 
 class ArticleCreate(LoginRequiredMixin, CreateView):
     """View for creating an article."""
-
     model = Article
     form_class = ArticleForm
     template_name = "common/forms/form.html"
@@ -61,9 +71,7 @@ class ArticleCreate(LoginRequiredMixin, CreateView):
 
 class SubarticleCreate(SlugPathMixin, BreadcrumbsMixin, ArticleCreate):
     """View for creating a subarticle."""
-
-    def get_breadcrumbs(self) -> list:
-        return self.get_object().breadcrumbs(include_self=True)
+    breadcrumb_parent = ArticleDetail
 
     def get_initial(self):
         parent = self.get_object()
@@ -72,30 +80,31 @@ class SubarticleCreate(SlugPathMixin, BreadcrumbsMixin, ArticleCreate):
             "public": parent.public,
             "comments_allowed": parent.comments_allowed,
         }
+    
+    def get_breadcrumbs_kwargs(self):
+        return {"article": self.get_object()}
 
 
 class ArticleUpdate(
-    PermissionOrCreatedMixin, BreadcrumbsMixin, SlugPathMixin, UpdateView
+    PermissionOrCreatedMixin, SlugPathMixin, BreadcrumbsMixin, UpdateView
 ):
     """View for updating a article."""
-
     model = Article
     form_class = ArticleForm
     template_name = "common/forms/form.html"
     permission_required = "articles.change_article"
+    breadcrumb_parent = ArticleDetail
 
-    def get_breadcrumbs(self) -> list:
-        return self.object.breadcrumbs(include_self=True)
+    def get_breadcrumbs_kwargs(self):
+        return {"article": self.object}
 
 
 class ArticleDelete(
-    PermissionOrCreatedMixin, BreadcrumbsMixin, SlugPathMixin, DeleteViewCustom
+    PermissionOrCreatedMixin, SlugPathMixin, BreadcrumbsMixin, DeleteViewCustom
 ):
     model = Article
     permission_required = "articles.delete_article"
-
-    def get_breadcrumbs(self) -> list:
-        return self.object.breadcrumbs(include_self=True)
+    breadcrumb_parent = ArticleDetail
 
     def get_success_url(self) -> str:
         return (
@@ -103,3 +112,6 @@ class ArticleDelete(
             if self.object.parent
             else reverse("dashboard:Dashboard")
         )
+    
+    def get_breadcrumbs_kwargs(self):
+        return {"article": self.object}
